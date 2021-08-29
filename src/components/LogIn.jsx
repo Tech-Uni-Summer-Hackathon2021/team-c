3d1161a0b0df0fe56ff8ba338db1ff0df3c097b2
田中篤 <atsusystem2118@gmail.com>
2021-08-29 21:32:31 +0900

    test

---
+++ src/components/LogIn.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function LogIn() {
+  return (
+    <View>
+      <Text>
+        ログイン画面
+      </Text>
+    </View>
+  );
+}
