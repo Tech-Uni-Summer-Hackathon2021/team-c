f29c92706e739760ebfabd40b2c9d6086b282c70
田中篤 <atsusystem2118@gmail.com>
2021-08-30 09:02:34 +0900

    てすと

--- my-app/App.jsx
+++ my-app/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Hello</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+});
