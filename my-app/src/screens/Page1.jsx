e2b2000e93547873a77bfdbcda385b952dc335ed
田中篤 <atsusystem2118@gmail.com>
2021-09-01 20:33:17 +0900

    ヘッダーとフッター作成．

--- my-app/src/screens/Page1.jsx
+++ my-app/src/screens/Page1.jsx
@@ -6,16 +6,25 @@ import {
 export default function Page1({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>このページはPage１です</Text>
-      <Button title="Page2へ" onPress={() => navigation.navigate('Page2')} />
+      <Text style={styles.text}>KOREDAKE</Text>
+      <Button title="ここにタスクを表示" onPress={() => navigation.navigate('Page2')} />
+      <Text style={styles.time}>00:00:00</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#F0F8FF',
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    paddingBottom: 90,
+  },
+  text: {
+    fontSize: 40,
+  },
+  time: {
+    fontSize: 20,
   },
 });
