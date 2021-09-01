a6f3af98cb6a7d13c9748d741002fff85c3b891e
田中篤 <atsusystem2118@gmail.com>
2021-09-01 14:13:27 +0900

    フッターを作成．

--- my-app/src/components/Footer.jsx
+++ my-app/src/components/Footer.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
 import { Appbar } from 'react-native-paper';
+import { Ionicons, FontAwesome } from '@expo/vector-icons';
 
 export default function Header() {
   return (
-    <Appbar.Header>
-      <Appbar.BackAction />
-      <Appbar.Content title="Title" subtitle="Subtitle" />
-      <Appbar.Action icon="magnify" />
-      <Appbar.Action icon="dots-vertical" />
+    <Appbar.Header style={styles.header}>
+      <Ionicons name="people" size={50} color="white" style={styles.peopleIcon} />
+      <FontAwesome name="home" size={50} color="white" style={styles.peopleIcon} />
     </Appbar.Header>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  peopleIcon: {
+    paddingHorizontal: 35,
+    paddingVertical: 10,
+  },
+});
