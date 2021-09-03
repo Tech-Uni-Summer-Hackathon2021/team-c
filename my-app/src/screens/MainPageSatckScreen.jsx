47af7009626f20997caccb4d485ce185d7c59690
田中篤 <atsusystem2118@gmail.com>
2021-09-04 08:30:35 +0900

    ESLintの修正．

--- my-app/src/screens/MainPageSatckScreen.jsx
+++ my-app/src/screens/MainPageSatckScreen.jsx
@@ -1,13 +1,7 @@
 import React from 'react';
-import { Button, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons';
 import firebase from 'firebase';
-import MyPageScreen from './MyPageScreen';
 import MainPageScreen from './MainPageScreen';
-import MemberlistScreen from './MemberListScreen';
 import MemberProfileScreen from './MemberProfileScreen';
 import LogInScreen from './LogInScreen';
 import SignUpScreen from './SignUpScreen';
