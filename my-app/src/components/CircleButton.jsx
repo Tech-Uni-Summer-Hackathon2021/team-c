c9430ff61599d6e7409daf4ee1701e123d33c9a0
junya942 <junyaooishi5@gmail.com>
2021-09-03 19:52:34 +0900

    main-pageのサンプル作成

---
+++ my-app/src/components/CircleButton.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { StyleSheet, TouchableOpacity } from 'react-native';
+import { shape, func } from 'prop-types';
+import { Feather } from '@expo/vector-icons';
+
+export default function CircleButton(props) {
+  const { style, onPress } = props;
+  return (
+    <TouchableOpacity style={[styles.circleButton, style]} onPress={onPress}>
+      <Feather size={32} color="white" />
+    </TouchableOpacity>
+  );
+}
+
+CircleButton.propTypes = {
+  style: shape(),
+  onPress: func,
+};
+
+CircleButton.defaultProps = {
+  style: null,
+  onPress: null,
+};
+
+const styles = StyleSheet.create({
+  circleButton: {
+    backgroundColor: '#467FD3',
+    width: 30,
+    height: 30,
+    borderRadius: 32,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    shadowColor: '#000000',
+    shadowOffset: { width: 0, height: 8 },
+  },
+  circleButtonLabel: {
+    color: '#ffffff',
+    fontSize: 20,
+    lineHeight: 40,
+  },
+});
--- my-app/src/screens/MainPageScreen.jsx
+++ my-app/src/screens/MainPageScreen.jsx
@@ -1,20 +1,119 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 
+import CircleButton from '../components/CircleButton';
+
 export default function MainPageScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>メインページ</Text>
+      <View>
+        <Text style={styles.topHome}>ホーム</Text>
+      </View>
+      <View>
+        <View style={styles.honmaContent}>
+          <View style={{ alignItems: 'center' }}>
+            <Text style={styles.text}>2021</Text>
+            <Text style={styles.textA}>8/13</Text>
+            <Text style={styles.text}>Fri.</Text>
+            <View style={{ flexDirection: 'row' }}>
+              <Text style={styles.textB}>一</Text>
+              <Text style={styles.textC}>今日のタスク</Text>
+              <Text style={styles.textB}>一</Text>
+            </View>
+            <Text style={styles.textD}>デザイン作成</Text>
+            <Text style={styles.textE}>00:00:00</Text>
+            <View>
+              <Text>某</Text>
+            </View>
+          </View>
+          <View style={{ flexDirection: 'row' }}>
+            <Text style={styles.TextF}>0%</Text>
+            <Text style={styles.TextF}>100%</Text>
+          </View>
+        </View>
+      </View>
+      <CircleButton style={{
+        right: 100,
+        bottom: 185,
+      }}
+      />
+      <CircleButton style={{
+        right: 140,
+        bottom: 185,
+      }}
+      />
+      <CircleButton style={{
+        right: 180,
+        bottom: 185,
+      }}
+      />
+      <CircleButton style={{
+        left: 100,
+        top: 185,
+      }}
+      />
+      <CircleButton style={{
+        left: 140,
+        top: 185,
+      }}
+      />
+      <CircleButton style={{
+        left: 180,
+        top: 185,
+      }}
+      />
     </View>
   );
 }
 const styles = StyleSheet.create({
+  topHome: {
+    bottom: 180,
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textA: {
+    fontSize: 14,
+    color: '#C0C0C0',
+  },
+  textC: {
+    padding: 17,
+    fontSize: 14,
+    color: '#C0C0C0',
+  },
+  textB: {
+    paddingTop: 10,
+    paddingRight: 0,
+    fontSize: 30,
+    color: '#000080',
+  },
+  textD: {
+    fontSize: 20,
+    color: '#000080',
+    textDecorationLine: 'underline',
+  },
+  textE: {
+    fontSize: 40,
+    color: '#C0C0C0',
   },
   text: {
-    fontSize: 30,
-    paddingVertical: 30,
+    fontSize: 10,
+    color: '#C0C0C0',
+  },
+  TextF: {
+    fontSize: 7,
+    justifyContent: 'space-between',
+  },
+  honmaContent: {
+    flexDirection: 'column',
+    paddingTop: 10,
+    paddingLeft: 10,
+    paddingRight: 10,
+    borderColor: 'rgba(0,0,0,0.2)',
+    backgroundColor: '#FFFFFF',
+    borderWidth: 1,
+    borderRadius: 2,
   },
 });
